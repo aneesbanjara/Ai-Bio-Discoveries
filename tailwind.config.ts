75a1bcf5d6bc14b0d6e1864437a2ef33545dc8bb
Anish <aneesbanjara488@gmail.com>
2024-08-21 17:01:05 +0545

    Changes in next.config.mjs --added output for export

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        'bg-image': "url('/public/covid-virus-3d-modeling.jpg')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
